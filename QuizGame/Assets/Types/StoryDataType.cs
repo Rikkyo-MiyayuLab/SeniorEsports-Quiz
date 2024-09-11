def7cbcc581438789f8ceec80a6414dcb271206a
tsyu12345 <syukut@gmail.com>
2024-09-11 16:57:05 +0900

    【ストーリー制作ツール #20】開発ツールにストーリーデータ作成用のツールを追加。一旦仮完成。
    一部問題制作ツールと統合したい関数等あるが、コンフリクト回避のため今コミットでは見送り。

--- QuizGame/Assets/Types/StoryDataType.cs
+++ QuizGame/Assets/Types/StoryDataType.cs
@@ -1,95 +1,105 @@
 using System.Collections.Generic;
+using System;
 
 namespace StoryDataInterface
 {
+    [Serializable]
     public class StoryData
     {
         /// <summary>
         /// ストーリーの識別子
         /// </summary>
-        public string StoryId { get; set; }
+        public string StoryId;
 
         /// <summary>
         /// ストーリーの各シーンのリスト
         /// </summary>
-        public List<Scene> Scenes { get; set; }
+        public List<Scene> Scenes;
+    }
+
+    /// <summary>
+    /// ストーリー 1フレームを構成するオブジェクト
+    /// </summary>
+    [Serializable]
+    public class Scene
+    {
+        /// <summary>
+        /// シーンの背景画像ファイル名
+        /// </summary>
+        public string Background { get; set; }
 
         /// <summary>
-        /// ストーリー 1フレームを構成するオブジェクト
+        /// 音声トラック等
         /// </summary>
-        public class Scene
-        {
-            /// <summary>
-            /// シーンの背景画像ファイル名
-            /// </summary>
-            public string Background { get; set; }
+        public string audio { get; set; }
 
-            /// <summary>
-            /// 音声トラック等
-            /// </summary>
-            public string audio { get; set; } 
+        /// <summary>
+        /// シーンに登場するキャラクターのリスト
+        /// </summary>
+        public List<Character> Characters { get; set; }
 
-            /// <summary>
-            /// シーンに登場するキャラクターのリスト
-            /// </summary>
-            public List<Character> Characters { get; set; }
+        /// <summary>
+        /// セリフの表示方法 ("oneByOne" = 1文字ずつ, "instant" = 全て即時表示)
+        /// </summary>
+        public TextDisplayMode? TextDisplayMode { get; set; }
 
-            /// <summary>
-            /// セリフの表示方法 ("oneByOne" = 1文字ずつ, "instant" = 全て即時表示)
-            /// </summary>
-            public TextDisplayMode? TextDisplayMode { get; set; }
+        /// <summary>
+        /// ナレーションがある場合のテキスト
+        /// </summary>
+        public string Narration { get; set; }
 
-            /// <summary>
-            /// ナレーションがある場合のテキスト
-            /// </summary>
-            public string Narration { get; set; }
+        /// <summary>
+        /// ナレーションの表示方法 ("modal" = モーダル表示, "inline" = インライン表示)
+        /// </summary>
+        public NarrationDisplayMode? NarrationDisplayMode { get; set; }
+    }
 
-            /// <summary>
-            /// ナレーションの表示方法 ("modal" = モーダル表示, "inline" = インライン表示)
-            /// </summary>
-            public NarrationDisplayMode? NarrationDisplayMode { get; set; }
-        }
+    [Serializable]
+    public class Character
+    {
+        /// <summary>
+        /// キャラクターの一意の識別子
+        /// </summary>
+        public string Name;
 
-        public class Character
-        {
-            /// <summary>
-            /// キャラクターの一意の識別子
-            /// </summary>
-            public string Id { get; set; }
+        /// <summary>
+        /// キャラクターのスプライト画像ファイルパス
+        /// </summary>
+        public string ImageSrc;
 
-            /// <summary>
-            /// キャラクターのスプライト画像ファイル名
-            /// </summary>
-            public string Sprite { get; set; }
+        /// <summary>
+        /// GUID
+        /// </summary>
+        public string ImageGUID;
+        
+        /// <summary>
+        /// キャラクターの表示位置 (1～4)
+        /// </summary>
+        public int Position;
 
-            /// <summary>
-            /// キャラクターの位置 (1～4)
-            /// </summary>
-            public int Position { get; set; }
+        /// <summary>
+        /// TODO:キャラクターのアニメーション状態 ("idle", "talk", "thinking" など)
+        /// TODO:将来的にはアニメーションの制御も行いたい
+        /// </summary>
+        // public string AnimationState { get; set; }
 
-            /// <summary>
-            /// キャラクターのアニメーション状態 ("idle", "talk", "thinking" など)
-            /// </summary>
-            public string AnimationState { get; set; }
+        /// <summary>
+        /// キャラクターのセリフ
+        /// </summary>
+        public string Dialogue;
+    }
 
-            /// <summary>
-            /// キャラクターのセリフ
-            /// </summary>
-            public string Dialogue { get; set; }
-        }
+    // Enum for text display mode
+    public enum TextDisplayMode
+    {
+        OneByOne,
+        Instant
+    }
 
-        // Enum for text display mode
-        public enum TextDisplayMode
-        {
-            OneByOne,
-            Instant
-        }
-
-        // Enum for narration display mode
-        public enum NarrationDisplayMode
-        {
-            Modal,
-            Inline
-        }
+    // Enum for narration display mode
+    public enum NarrationDisplayMode
+    {
+        Modal,
+        Inline
     }
 }
