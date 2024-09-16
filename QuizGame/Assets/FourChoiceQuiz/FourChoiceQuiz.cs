5f0b4c29f10174e121e05cd3bc3379ffc85c832e
tsyu1234 <syukunt@gmail.com>
2024-09-17 00:19:28 +0900

    【4択式解答画面 #36】 4択式解答画面の仮完成。

--- QuizGame/Assets/FourChoiceQuiz/FourChoiceQuiz.cs
+++ QuizGame/Assets/FourChoiceQuiz/FourChoiceQuiz.cs
@@ -10,47 +10,92 @@ using Newtonsoft.Json;
 using PlayType1Interface;
 using TMPro;
 using QuestionDataInterface;
+using EasyTransition;
 
 /// <summary>
-/// 4択式の解答画面を表示するクラス
+/// 4択式の解答画面を表示するクラス 
 /// UI構成は１問ずつとし、小問データを切替えることで問題を切り替える。
-/// TODO: 正解判定処理
-/// TODO: 解答マスが複数ある場合、次の解答マスへ入力フォーカスを移動する処理
-/// TODO: 次小問への遷移処理
-/// TODO: 正解or不正解リザルト画面の表示（仮表示）
-/// TODO: タイプ３対応、任意画像の読み込み・出力対応
-/// TODO: 全ての小問を終えた後、解説用ストーリー画面へ遷移する処理
+/// TODO: 解答ボタン選択時の効果音、エフェクト
+/// TODO: BGMの設定
+/// TODO: リザルト時の効果音、エフェクト
+/// TODO: 統合用親クラスの設置 
 /// </summary>
 public class FourChoiceQuiz : MonoBehaviour {
 
     public int currentQuestionIndex = 0; // 現在の小問のインデックス
     public int currentAnswerCellIdx = 0; //現在の解答マスのインデックス
-    public string QuestionID;
     public Sprite BackgroundImg;
+    public Sprite QuestionImg;
     public List<List<Cell>> Grids;
     public List<List<GameObject>> GridObjects;
     public Action<bool> OnAnswered;
     [SerializeField]
     public List<List<Option>> AnswerOptions;
     public List<Button> AnswerButtonObjects;
-    [SerializeField]
-    private string JSONPath;
-    [SerializeField]
-    private GameObject questionArea;
+    public TransitionSettings transition;
+    public float transitionDuration = 0.5f;
     [SerializeField]
     private GameObject GridsArea;
     [SerializeField]
-    private GameObject AnswerGridArea;
-    [SerializeField]
     private Image backgroundImageObj;
+    [SerializeField]
+    private GameObject QuestionImageObj;
+    [SerializeField]
+    private Canvas ResultModal;
+    [SerializeField]
+    private Button RetryButton;
+    [SerializeField]
+    private Button NextButton;
+    [SerializeField]
+    private Image ResultModalImage;
+    private TransitionManager transitionManager;
     private Question questionData;
     private QuestionData allQuestionData;
+    private List<bool> correctness = new List<bool>();
     
 
 
     void Start() {
+        GridObjects = new List<List<GameObject>>();
+        AnswerOptions = new List<List<Option>>();
+        transitionManager = TransitionManager.Instance();
         GetData();
         Init();
+        // 解答用ボタンにイベントリスナーを設定
+        List<Option> options = AnswerOptions[currentAnswerCellIdx]; // currentAnswerCellIdx に該当するオプションのリストを取得
+        for (int i = 0; i < AnswerButtonObjects.Count; i++) {
+            // ラムダ式の中で現在のインデックス i をキャプチャ
+            int btnIdx = i;  
+            AnswerButtonObjects[i].onClick.AddListener(() => {
+                var isCorrect = options[btnIdx].correct;  // optionsのインデックスを使って選択肢を取得
+                if (isCorrect) {
+                    OnAnswered?.Invoke(true);
+                } else {
+                    OnAnswered?.Invoke(false);
+                }
+            });
+        }
+        OnAnswered += AnswerQuestionHandler;
+
+        RetryButton.onClick.AddListener(() => {
+            ResultModal.gameObject.SetActive(false);
+            //TODO : 現在の小問をリトライする処理
+        });
+        
+        NextButton.onClick.AddListener(() => {
+            ResultModal.gameObject.SetActive(false);
+            if(currentQuestionIndex < allQuestionData.quiz.questions.Count - 1) { // 次問遷移
+                NextQuestion();
+            } else { // 大問終了
+                transitionManager.Transition(transition, transitionDuration);
+                transitionManager.onTransitionEnd = () => {
+                    //TODO : 全ての小問を終えた後、解説用ストーリー画面へ遷移する処理
+                    // ここで、ストーリーIDを指定して、ストーリー用シーンへ遷移する
+                    // Ex). SceneManager.LoadScene("StoryScene");
+                    Debug.Log("大問終了遷移");
+                };
+            }
+        });
     }
 
     public void GetData() {
@@ -61,30 +106,24 @@ public class FourChoiceQuiz : MonoBehaviour {
     }
 
     public void Init() {
+        Dispose();
         BackgroundImg = Resources.Load<Sprite>(questionData.backgroundImage);
         backgroundImageObj.sprite = BackgroundImg;
 
+        if(questionData.questionImage != null) {
+            QuestionImageObj.SetActive(true);
+            QuestionImg = Resources.Load<Sprite>(questionData.questionImage);
+            QuestionImageObj.GetComponent<SpriteRenderer>().sprite = QuestionImg;
+        } else {
+            QuestionImageObj.SetActive(false);
+        }
+
         GenerateGrids(questionData);
         // 解答用ボタンに選択肢を設定
-        foreach(var option in AnswerOptions) {
-            for(int i = 0; i < AnswerButtonObjects.Count; i++) {
-                AnswerButtonObjects[i].GetComponentInChildren<TextMeshProUGUI>().text = option[i].text;
-            }
-        }
-
-        // 解答用ボタンにイベントリスナーを設定
+        List<Option> options = AnswerOptions[currentAnswerCellIdx]; // currentAnswerCellIdx に該当するオプションのリストを取得
         for (int i = 0; i < AnswerButtonObjects.Count; i++) {
-            AnswerButtonObjects[i].onClick.AddListener(() => {
-                var isCorrect = AnswerOptions[currentAnswerCellIdx][i].correct;
-                if (isCorrect) {
-                    OnAnswered?.Invoke(true);
-                } else {
-                    OnAnswered?.Invoke(false);
-                }
-            });
+            AnswerButtonObjects[i].GetComponentInChildren<TextMeshProUGUI>().text = options[i].text;
         }
-
-        OnAnswered += AnswerQuestionHandler;
     }
     
     /// <summary>
@@ -97,11 +136,9 @@ public class FourChoiceQuiz : MonoBehaviour {
     public void GenerateGrids(Question questionData) {
         /** 問題用のグリッドを生成 */
         Grids = questionData.grids; 
-        GridObjects = new List<List<GameObject>>();
         GridsArea.transform.localScale = new Vector3(1, 1, 1);
         GridsArea.transform.localPosition = new Vector3(questionData.gridsPos[0], questionData.gridsPos[1], 0);
         GridsArea.transform.localScale = new Vector3(questionData.gridsScale, questionData.gridsScale, 1);
-        AnswerOptions = new List<List<Option>>();
         for (int i = 0; i < Grids.Count; i++) {
             List<GameObject> gridObjects = new List<GameObject>();
             for (int j = 0; j < Grids[i].Count; j++) {
@@ -123,29 +160,62 @@ public class FourChoiceQuiz : MonoBehaviour {
         }
     }
 
-    private void AnswerQuestionHandler(bool correctedQuestion) {
+
+    private void Dispose() {
+        ResultModal.gameObject.SetActive(false);
+        foreach (var gridObjects in GridObjects) {
+            foreach (var gridObject in gridObjects) {
+                Destroy(gridObject);
+            }
+        }
+        GridObjects.Clear();
+
+        for (int i = 0; i < AnswerButtonObjects.Count; i++) {
+            AnswerButtonObjects[i].GetComponentInChildren<TextMeshProUGUI>().text = "";
+        }
+        AnswerOptions.Clear();
+
+        if (QuestionImageObj.activeSelf) {
+            QuestionImageObj.SetActive(false);
+        }
+
+    }
+
+    private void AnswerQuestionHandler(bool isCorrectedQuestion) {
         
         if(currentAnswerCellIdx < AnswerOptions.Count - 1) { //まだ解答マスがある場合は次の解答マスへフォーカスを移動
             currentAnswerCellIdx++;
+            correctness.Add(isCorrectedQuestion);
+            // 選択肢のボタンテキストを更新
+            List<Option> options = AnswerOptions[currentAnswerCellIdx]; // currentAnswerCellIdx に該当するオプションのリストを取得
+            for (int i = 0; i < AnswerButtonObjects.Count; i++) {
+                AnswerButtonObjects[i].GetComponentInChildren<TextMeshProUGUI>().text = options[i].text;
+            }
         } else { // 小問解答を終えた場合
             //TODO : 小問リザルト画面を表示
-            if(correctedQuestion) {
-
+            ResultModal.gameObject.SetActive(true);
+            if(correctness.TrueForAll(x => x)) {
+                Debug.Log("正解");
+                //TODO : 正解用のイメージ画像を表示
             } else {
-
-            }
-
-            // 次問遷移 or 大問終了
-            if(currentQuestionIndex < allQuestionData.quiz.questions.Count - 1) {
-                currentQuestionIndex++;
-                questionData = LoadJSON<Question>(allQuestionData.quiz.questions[currentQuestionIndex]);
-                Init();
-            } else {
-                // TODO:全ての小問を終えた後、解説用ストーリー画面へ遷移
-                // ここで、ストーリーIDを指定して、ストーリー用シーンへ遷移する
-                // Ex). SceneManager.LoadScene("StoryScene");
+                Debug.Log("不正解");
+                RetryButton.gameObject.SetActive(true);
+                //TODO : 不正解用のイメージ画像を表示
             }
         }
+
+        // 全問解き終えたか？
+        if(currentQuestionIndex < allQuestionData.quiz.questions.Count - 1) {
+            ResultModal.gameObject.SetActive(true);
+            // 戻るボタンを表示
+            RetryButton.gameObject.SetActive(false);
+        }
+    }
+
+    private void NextQuestion() {
+        currentQuestionIndex++;
+        questionData = LoadJSON<Question>(allQuestionData.quiz.questions[currentQuestionIndex]);
+        Init();
     }
 
     /// <summary>
