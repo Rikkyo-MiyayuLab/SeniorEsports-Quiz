ed0aad85859cba51aa00512041c739006246a1bf
tsyu1234 <syukunt@gmail.com>
2024-09-27 00:06:32 +0900

    【プレビュー版 #35】 エリア画面の装飾。

--- QuizGame/Assets/Mode/AreaMap.cs
+++ QuizGame/Assets/Mode/AreaMap.cs
@@ -1,18 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class AreaMap : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Button[] AreaButtons;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private int CurrentAreaIdx; // PlayerPrefから取得する現在地情報
+
+    // ステータスアイコンのスプライトを事前にキャッシュ
+    private Sprite statusIconCurrent;
+    private Sprite statusIconLocked;
+
+    void Start() {
+        // スプライトを事前にキャッシュしておく
+        statusIconCurrent = Resources.Load<Sprite>("System/nazo_icon");
+        statusIconLocked = Resources.Load<Sprite>("System/lock_icon");
+
+        // PlayerPrefsから現在のエリアインデックスを取得、デフォルト値は0
+        CurrentAreaIdx = PlayerPrefs.GetInt("CurrentAreaIdx", 0);
+
+        for (int i = 0; i < AreaButtons.Length; i++) {
+            // ボタンの最初の子要素を取得 (charactorImg)
+            GameObject charactorImg = AreaButtons[i].transform.GetChild(0).gameObject;
+
+            // 子要素から StatusIcon を取得
+            Image statusIcon = charactorImg.transform.GetChild(0).gameObject.GetComponent<Image>();
+
+            if (i == CurrentAreaIdx) {
+                // 現在地のボタンは点滅し、ステータスアイコンを変更
+                AreaButtons[i].GetComponent<ButtonBlink>().StartBlinking();
+                statusIcon.sprite = statusIconCurrent;  // 現在地のアイコンに変更
+            } else {
+                // 他のボタンは点滅を停止し、ステータスアイコンをロック状態に設定
+                AreaButtons[i].GetComponent<ButtonBlink>().StopBlinking();
+                statusIcon.sprite = statusIconLocked;  // ロック状態のアイコンに変更
+
+                // ボタンをクリック不可にする
+                AreaButtons[i].interactable = false;
+            }
+        }
     }
 }
