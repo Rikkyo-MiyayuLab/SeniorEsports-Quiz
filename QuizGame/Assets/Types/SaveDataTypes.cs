93f7f64bdaa2757ec8d8a8b4e282e616ebc477cb
tsyu12345 <syukut@gmail.com>
2024-10-02 20:50:25 +0900

    【ベータ１】ゲーム内でユーザーデータを扱うAPI、インターフェースの追加。#47 #60

---
+++ QuizGame/Assets/Types/SaveDataTypes.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+
+namespace SaveDataInterface {
+
+    public class PlayerData {
+        /// <summary>
+        /// プレイヤーデータ識別用UUID
+        /// </summary>
+        public string PlayerUUID;
+        /// <summary>
+        /// プレイヤーの表示名
+        /// </summary>
+        public string PlayerName;
+        /// <summary>
+        /// 総プレイ時間 sec
+        /// </summary>
+        public float TotalPlayTime;
+        /// <summary>
+        /// 解決済みの問題数
+        /// </summary>
+        public int TotalResolvedCount;
+        /// <summary>
+        /// 現在のワールドマップの場所(最進捗) 
+        /// </summary>
+        public string CurrentWorld;
+        public string CurrentArea;
+        /// <summary>
+        /// 最後にプレイしたストーリーID
+        /// </summary>
+        public string LastStoryId;
+        public int UserAge;
+    }
+
+    public enum PlayerPrefKeys {
+        PlayerName,
+        TotalPlayTime,
+        TotalResolvedCount,
+        CurrentWorld,
+        CurrentArea,
+        LastStoryId,
+        UserAge,
+    }
+}
+
