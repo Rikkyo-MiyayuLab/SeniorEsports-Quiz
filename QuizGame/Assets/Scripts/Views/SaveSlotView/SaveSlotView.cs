cdef75cc60d97f16f066f8f04fd0e91ff10db429
SO高林秀 <s.takabayashi@super-one.co.jp>
2025-03-24 00:19:40 +0900

    【v2.0.0-beta1】ユーザースロット画面のフォント乱れ修正 & 成績確認画面から戻れない問題の修正 #97

--- QuizGame/Assets/Scripts/Views/SaveSlotView/SaveSlotView.cs
+++ QuizGame/Assets/Scripts/Views/SaveSlotView/SaveSlotView.cs
@@ -60,7 +60,8 @@ public class SaveSlotView : MonoBehaviour {
             
             // 各Text要素を取得し、PlayerDataの情報を表示
             slot.transform.Find("UserName").GetComponent<TextMeshProUGUI>().text = playerData.PlayerName;
-            slot.transform.Find("LastPlayedDate").GetComponent<TextMeshProUGUI>().text = playerData.LastPlayDate;
+            // YY-MM-DD hh:mm:ss の形式のうち、YY-MM-DDの部分を取得して表示
+            slot.transform.Find("LastPlayedDate").GetComponent<TextMeshProUGUI>().text = playerData.LastPlayDate.Split(' ')[0];
             // TotalPlayTimeはsecなので、日時間分に変換
             int[] timeParts = DateTimeUtils.ConvertSecToHHMMSS(playerData.TotalPlayTime);
             slot.transform.Find("TotalPlayTime").GetComponent<TextMeshProUGUI>().text = $"{timeParts[0]}時間{timeParts[1]}分{timeParts[2]}秒";
